fb0aa29beb054dfbb407f0a20cd72ce1d2d98b78
Sarvesh <sambhachoubey@gmail.com>
2026-07-17 21:01:44 +0530

    Add MGID homepage and sidebar widgets

--- src/components/ads/MGID/Config.ts
+++ src/components/ads/MGID/Config.ts
@@ -1,7 +1,7 @@
 export const MGID_WIDGETS = {
-  articleTop: "2055717",
-  articleBottom: "2055717",
-  homepage: "2055717",
+  articleTop: "2055811",
+  articleBottom: "2055818",
+  homepage: "2055819",
   category: "2055717",
-  sidebar: "2055717",
+  sidebar: "2055823",
 };
